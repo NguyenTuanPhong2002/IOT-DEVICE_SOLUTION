dfcfe7eae7f1e7e25bd3ad3b20caf1761f2d244e
d-Phap_p <nguyenducphap199924558@gmail.com>
2023-10-10 22:40:19 +0700

    xu ly GPS

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <Arduino.h>
+#include <TinyGPSPlus.h>
 
+
+TinyGPSPlus gps;
 //define pins
 #define TRIG_PIN 23
 #define ECHO_PIN 22
@@ -30,12 +33,35 @@ float measure_distance() {
 void setup() {
     // Initialize serial communication
     Serial.begin(9600);
+    Serial2.begin(9600);
+    delay(3000);
 
     // Set pin modes
     pinMode(TRIG_PIN, OUTPUT);
     pinMode(ECHO_PIN, INPUT);
 }
-
+void updateSerial(){
+  delay(500);
+  while (Serial.available())  {
+    Serial2.write(Serial.read());//Forward what Serial received to Software Serial Port
+  }
+  while (Serial2.available())  {
+    Serial.write(Serial2.read());//Forward what Software Serial received to Serial Port
+  }
+}
+void displayInfo()
+{
+  Serial.print(F("Location: "));
+  if (gps.location.isValid()){
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+}
 void loop() {
     // Measure distance and print to serial monitor
     float distance = measure_distance();
@@ -45,4 +71,14 @@ void loop() {
 
     // Wait for 1 second before measuring again
     delay(1000);
+
+    //updateSerial();
+    while (Serial2.available() > 0)
+    if (gps.encode(Serial2.read()))
+        displayInfo();
+    if (millis() > 5000 && gps.charsProcessed() < 10)
+    {
+    Serial.println(F("No GPS detected: check wiring."));
+    while (true);
+  }
 }
